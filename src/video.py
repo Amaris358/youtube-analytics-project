ced25261de771293fd68f11ffcbb51bbe67867e3
Anastasiya <puchinina.anastasia58@gmail.com>
2023-12-18 00:52:13 +0700

    Added IndexError

--- src/video.py
+++ src/video.py
@@ -2,14 +2,25 @@ from googleapiclient.discovery import build
 
 
 class Video:
+    api_key = 'AIzaSyCN7BT8XlSAdIBsCM4FqxqwgfX87CGaH-g'
+    youtube = build("youtube", "v3", developerKey=api_key)
+
     def __init__(self, video_id):
         self.video_id = video_id
-        self.youtube = build("youtube", "v3", developerKey='AIzaSyCN7BT8XlSAdIBsCM4FqxqwgfX87CGaH-g')
-        self.video_full_info = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails', id=self.video_id).execute()
-        self.title = self.video_full_info['items'][0]['snippet']['title']
-        self.url = "https://youtu.be/" + self.video_id
-        self.view_count = self.video_full_info['items'][0]['statistics']['viewCount']
-        self.like_count = self.video_full_info['items'][0]['statistics']['likeCount']
+        try:
+            Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                        id=self.video_id).execute()
+            self.video_full_info = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                               id=self.video_id).execute()
+            self.title = self.video_full_info['items'][0]['snippet']['title']
+            self.url = "https://youtu.be/" + self.video_id
+            self.view_count = self.video_full_info['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_full_info['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return self.title
